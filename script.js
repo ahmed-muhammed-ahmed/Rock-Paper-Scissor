5253e79e3e9af32f1059e80f203e1668d66b158e
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2023-11-30 14:28:22 +0300

    Refactor game loop and scoring logic for clarity and conciseness
    In this commit, I've enhanced the game loop and scoring logic within
    the game function. The loop now iterates five times, prompting the user
    for their choice and playing a round using the playRound function.
    The scoring has been streamlined, and the outcomes are logged to the console.
    
    The scoring logic now checks for "win" or "lose" in the score string,
    updating the playerScore or computerScore accordingly. Finally,
    the results of the five rounds are summarized, and
    the winner or a tie is reported at the end of the game.

--- script.js
+++ script.js
@@ -1,63 +1,81 @@
 "use strict";
 
-const choice = Math.trunc(Math.random() * 3);
-const listOfChoice = ["Rock", "Paper", "Scissor"];
-function getComputerChoice(computerChoice) {
-    return listOfChoice[computerChoice];
+function getComputerChoice() {
+    const choice = Math.trunc(Math.random() * 3);
+    const listOfChoice = ["Rock", "Paper", "Scissor"];
+    return listOfChoice[choice];
 }
 
 
-let playerChoice = prompt("What is your choice? Rock, Paper, Scissor");
 
-function getPlayerChoice(playerChoice) {
-    return playerChoice;
-}
+let computerSelection = getComputerChoice();
+let playerSelection = "Paper";
+playerSelection = playerSelection.toLowerCase();
 
-const computerSelection = getComputerChoice(choice);
-const playerSelection = getPlayerChoice(playerChoice);
+// Validate user input
+const validChoices = ["rock", "paper", "scissor"];
 
-// console.log(computerSelection);
-// console.log(playerSelection);
+
 let winner = "";
 
 function playRound(playerSelection, computerSelection) {
-    if((computerSelection === "Rock") && 
-    (playerSelection === "Scissor" || 
-    playerSelection.toUpperCase()=== "SCISSOR" || 
-    playerSelection.toLowerCase() === "scissor")) {
+    if((computerSelection.toLowerCase() === "rock") && (playerSelection.toLowerCase() === "scissor")) {
         winner = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    } else if((computerSelection === "Rock") && 
-    (playerSelection === "Paper" ||
-    playerSelection.toUpperCase() === "PAPER" ||
-    playerSelection.toLowerCase() === "paper")) {
+    } else if((computerSelection.toLowerCase() === "rock") && (playerSelection.toLowerCase() === "paper")) {
         winner = `You win! ${playerSelection} beats ${computerSelection}.`;
-    } else if((computerSelection === "Scissor") && 
-    (playerSelection === "Rock" || 
-    playerSelection.toUpperCase() === "ROCK" || 
-    playerSelection.toLowerCase() === "rock")) {
+    } else if((computerSelection.toLowerCase() === "scissor") && (playerSelection.toLowerCase() === "rock")) {
         winner = `You win! ${playerSelection} beats ${computerSelection}`;
-    } else if ((computerSelection === "Scissor") && 
-    (playerSelection === "Paper" ||
-    playerSelection.toUpperCase() === "PAPER" ||
-    playerSelection.toLowerCase() === "paper")) {
+    } else if ((computerSelection.toLowerCase() === "scissor") && (playerSelection.toLowerCase() === "paper")) {
         winner = `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else if((computerSelection === "Paper") && 
-    (playerSelection === "Rock" || 
-    playerSelection.toUpperCase() === "ROCK" || 
-    playerSelection.toLowerCase() === "rock")) {
+    } else if((computerSelection.toLowerCase() === "paper") && (playerSelection.toLowerCase() === "rock")) {
         winner = `You lose! ${computerSelection} beats ${playerSelection}!`;
-    } else if((computerSelection === "Paper") && 
-    (playerSelection === "Scissor" || 
-    playerSelection.toUpperCase()=== "SCISSOR" || 
-    playerSelection.toLowerCase() === "scissor")) {
+    } else if((computerSelection.toLowerCase() === "paper") && (playerSelection.toLowerCase() === "scissor")) {
         winner = `You win! ${playerSelection} beats ${computerSelection}`
-    } else {
+    } else if(!validChoices.includes(playerSelection)) {
+        alert("Invalid input. Choose Rock, Paper, or Scissor");
+        playerSelection = prompt("What is your choice? Rock, Paper, Scissor").toLowerCase();
+    }
+     else {
         winner = `It's ties.`
     }
 
     return winner;
 }
 
-const score = playRound(playerSelection, computerSelection); 
-console.log(score);
+
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+   for(let i = 0; i < 5; i++) {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt("What is your choice? Rock, Paper, Scissor");
+    const score = playRound(playerSelection, computerSelection); 
+    console.log(score);
+     
+    if(score.includes("win") || score.includes("lose")){
+        if(score.includes("win")) {
+            playerScore += 1;
+            
+        } else {
+            computerScore += 1;
+
+        }
+
+     }
+
+   }
+
+   if(playerScore === computerScore) {
+     console.log(`It's ties! (Player Score: ${playerScore} vs Computer Score ${computerScore})`)
+   } else if(playerScore > computerScore) {
+    console.log(`You win by (Player Score: ${playerScore} vs Computer Score ${computerScore}) 🎉`)
+   } else {
+    console.log(`You lose by (Computer Score ${computerScore} vs Player Score: ${playerScore}) 😡`)
+   }
+
+}
+
+game();
 

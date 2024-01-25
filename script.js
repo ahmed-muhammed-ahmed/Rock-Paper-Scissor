0b5d7c351cc1ccc313740172afc2e4b297eaecb0
ahmedmuhammed <ahmedmuhammedem@gmail.com>
2024-01-25 13:54:32 +0300

    fix: Prevent displaying multiple results by updating event handling
    Previously, each button had an individual event listener, causing
    multiple results to be displayed without removing the previous ones.
    To address this, I refactored the code to select the entire
    'btn-container'
    
    Now, the 'btn-container' listens for clicks on its child elements,
    specifically the images representing Rock, Paper, and Scissors.
    Depending on which image is clicked, the corresponding result for both
    the player and computer is displayed. This prevents the accumulation of
    multiple results on the screen during successive clicks.
    
    Additionally, made improvements to the overall style and HTML structure
    of the game for a more polished appearance. Adjusted CSS styles to
    enhance visual elements and optimized the HTML structure for better
     maintainability and clarity.

--- script.js
+++ script.js
@@ -1,100 +1,62 @@
-"use strict";
+const computerChoice = document.querySelector("#computer-choice");
+const btnContainer = document.querySelector(".btn-container");
+const yourChoice = document.querySelector("#your-choice");
+const resultDisplay = document.querySelector("#result");
+let imgElement = document.createElement("img");
+let imgElement1 = document.createElement("img");
+let playerSelection = "";
+let winner = "";
+let result = 0;
+
+function playRound() {
+  btnContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
+      playerSelection = e.target.id;
+      imgElement.src = `./img/${e.target.id}.svg`;
+      // console.log(imgElement);
+      yourChoice.appendChild(imgElement);
+      // yourChoice.textContent = playerSelection;
+      getResult();
+    }
+  });
+}
+
+playRound();
 
 function getComputerChoice() {
   const choice = Math.trunc(Math.random() * 3);
-  const listOfChoice = ["Rock", "Paper", "Scissor"];
+  const listOfChoice = ["rock", "paper", "scissors"];
   return listOfChoice[choice];
 }
 
 let computerSelection = getComputerChoice();
+function getResult() {
+  computerSelection = getComputerChoice();
+  // computerChoice.textContent = computerSelection;
+  imgElement1.src = `./img/${computerSelection}.svg`;
+  computerChoice.appendChild(imgElement1);
+  if (computerSelection === playerSelection) {
+    winner = "It's ties";
+    result = 0;
+  } else if (computerSelection === "rock" && playerSelection === "paper") {
+    winner = `You win ${playerSelection} beats ${computerSelection}`;
+    result = 1;
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    winner = `You lost ${computerSelection} beats ${playerSelection}`;
+    result = -1;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    winner = `You lost ${computerSelection} beats ${playerSelection}`;
+    result = -1;
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    winner = `You win ${playerSelection} beats ${computerSelection}`;
+    result = 1;
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    winner = `You win ${playerSelection} beats ${computerSelection}`;
+    result = 1;
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    winner = `You lost ${computerSelection} beats ${playerSelection}`;
+    result = -1;
+  }
 
-// DOM PART
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissor = document.querySelector(".scissor");
-const result = document.querySelector(".result");
-const playerChoiceRock = document.getElementById("rock");
-const playerChoicePaper = document.getElementById("paper");
-const playerChoiceScissor = document.getElementById("scissor");
-const computerChoiceRock = document.getElementById("computer-rock");
-const computerChoicePaper = document.getElementById("computer-paper");
-const computerChoiceScissor = document.getElementById("computer-scissor");
-const plyScore = document.querySelector(".player-score");
-const compScore = document.querySelector(".computer-score");
-const replayBtn = document.querySelector(".btn-replay");
-
-function reset() {
-  replayBtn.classList.add("btn-replay");
-  result.textContent = "Let's play";
-  playerChoiceRock.style.display = "inline";
-  playerChoicePaper.style.display = "none";
-  playerChoiceScissor.style.display = "none";
-  computerChoiceRock.style.display = "inline";
-  computerChoicePaper.style.display = "none";
-  computerChoiceScissor.style.display = "none";
+  resultDisplay.textContent = winner;
 }
-
-function playRound() {
-  rock.addEventListener("click", function () {
-    computerSelection = getComputerChoice();
-    playerChoiceRock.style.display = "inline";
-    computerChoiceRock.style.display = "none";
-    replayBtn.classList.remove("btn-replay");
-    if (computerSelection === "Scissor") {
-      computerChoiceScissor.style.display = "inline";
-      result.textContent = "You win, rock beats scissor";
-    } else if (computerSelection === "Paper") {
-      computerChoicePaper.style.display = "inline";
-      result.textContent = "You lost, paper beats rock!";
-    } else {
-      result.textContent = "It's a ties";
-      computerChoiceRock.style.display = "inline";
-    }
-  });
-
-  paper.addEventListener("click", function () {
-    computerSelection = getComputerChoice();
-
-    playerChoiceRock.style.display = "none";
-    computerChoiceRock.style.display = "none";
-    playerChoicePaper.style.display = "inline";
-    replayBtn.classList.remove("btn-replay");
-
-    if (computerSelection === "Rock") {
-      result.textContent = "You win, paper beats rock";
-      computerChoiceRock.style.display = "inline";
-    } else if (computerSelection === "Scissor") {
-      result.textContent = "You lost, scissor beats paper";
-      computerChoiceScissor.style.display = "inline";
-    } else {
-      result.textContent = "It's a ties";
-      computerChoicePaper.style.display = "inline";
-    }
-  });
-
-  scissor.addEventListener("click", function () {
-    computerSelection = getComputerChoice();
-    playerChoiceRock.style.display = "none";
-    computerChoiceRock.style.display = "none";
-    playerChoiceScissor.style.display = "inline";
-    replayBtn.classList.remove("btn-replay");
-
-    if (computerSelection === "Paper") {
-      result.textContent = "You win scissor beats paper";
-
-      computerChoiceRock.style.display = "none";
-      computerChoicePaper.style.display = "inline";
-    } else if (computerSelection === "Rock") {
-      result.textContent = "You lost rock beats scissor";
-      computerChoiceRock.style.display = "inline";
-    } else {
-      result.textContent = "It's ties";
-      computerChoiceRock.style.display = "none";
-      computerChoiceScissor.style.display = "inline";
-    }
-  });
-
-  replayBtn.addEventListener("click", reset);
-}
-
-playRound();
